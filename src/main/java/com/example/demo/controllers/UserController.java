bda5260255aabba07b2b926e6387e968f4e1c0c4
AlexandruSamoila <Alexandru.Samoila@rinf.tech>
2023-02-03 09:22:28 +0200

    Revert "Added tests for UserServices."
    This reverts commit 5c78ebc98f98aee15295f8f9ab338b1b7bd364ec.

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -12,13 +12,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
